933ced13f5df5583158a0b3ea8613c4ba72089f1
spoonm <skewerr@users.noreply.github.com>
2015-05-21 08:32:55 -0300

    Changed command dictionary to an object.
    Instead of trying to access var.commands[".command"]["aliases"], it can now be done with var.commands[".command"].aliases.
    Also, added help info for the help command.

--- modules/CommandModules/help.py
+++ modules/CommandModules/help.py
@@ -1,5 +1,17 @@
 from .. import irc, var
 
+# Fill commands dictionary with usage.
+def ins_help ():
+    var.commands[".help"] = type("command", (object,), {})()
+    var.commands[".help"].aliases = [
+        ".help",
+        "!help",
+    ]
+    var.commands[".help"].usage = [
+        "{} - Display list of commands.",
+        "{} .command - Get additional help for that command."
+    ]
+
 # Call needed function.
 def read (user, channel, word):
     if len(word) == 1:
@@ -18,14 +30,14 @@ def command_help (user, command):
     
     # Fill dictionary of aliases. Link them to the command name.
     for cmd in var.commands:
-        for alias in var.commands[cmd]["aliases"]:
+        for alias in var.commands[cmd].aliases:
             dict[alias] = cmd
     
     if command not in dict:
         irc.notice(user, "No usage info for {} available.".format(command))
     else:
-        aliases = [alias for alias in dict if dict[alias] == dict[command]]
-        usage = var.commands[dict[command]]["usage"]
+        aliases = var.commands[dict[command]].aliases
+        usage = var.commands[dict[command]].usage
         
         irc.notice(user, "Command aliases: " + " ".join(aliases))
         irc.notice(user, "Usage:")
