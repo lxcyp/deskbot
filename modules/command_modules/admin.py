a07de8a1f5afffe3f21be1489f42ad51e466a13d
spoonm <skewerr@users.noreply.github.com>
2015-05-22 13:06:07 -0300

    Added command-specific exceptions.
    Added the enable and disable commands, which accept multiple commands and aliases.

--- modules/command_modules/admin.py
+++ modules/command_modules/admin.py
@@ -22,6 +22,17 @@ def ins_command ():
     var.commands["identify"].method = identify
     var.commands["identify"].aliases = [".identify", ".ident"]
     var.commands["identify"].usage = ["{} - Try to identify with NickServ using set password."]
+    
+    var.commands["disable"] = type("command", (object,), {})()
+    var.commands["disable"].method = disable
+    var.commands["disable"].aliases = [".disable", ".rm"]
+    var.commands["disable"].usage = ["{} .command1 .command2 ... - Disable commands for this channel."]
+    
+    var.commands["enable"] = type("command", (object,), {})()
+    var.commands["enable"].method = enable
+    var.commands["enable"].aliases = [".enable", ".add"]
+    var.commands["enable"].usage = ["{} .command1 .command2 ... - Enable commands for this channel."]
+
 
 # Require NickServ authentication for the admin.
 def ident (f):
@@ -56,3 +67,55 @@ def raw (user, channel, word):
 # Sometimes the bot fails to identify with NickServ.
 def identify (user, channel, word):
     irc.identify()
+
+# Disable commands. Can accept multiple commands.
+def disable (user, channel, word):
+    dsbl_list = [cmd for cmd in word if cmd not in [".disable", ".enable"]]
+    disabled = []
+    
+    # If there's nothing it can disable...
+    if not dsbl_list:
+        irc.msg(channel, "{}: No commands to disable.".format(user))
+        return
+    
+    # Add channel to the command object disabled attribute, if not already in it.
+    for command in dsbl_list:
+        for cmd_name in var.commands:
+            if command in var.commands[cmd_name].aliases:
+                if channel in var.commands[cmd_name].disabled:
+                    irc.notice(user, "{} is already disabled in this channel.".format(command))
+                else:
+                    var.commands[cmd_name].disabled.append(channel)
+                    disabled.append(command)
+    
+    # Phew. Now check if anything was indeed disabled and what.
+    if not disabled:
+        irc.msg(channel, "{}: No commands were disabled.".format(user))
+    else:
+        irc.msg(channel, "{}: Disabled commands: {}".format(user, " ".join(disabled)))
+
+# Enable commands. Can accept multiple commands.
+def enable (user, channel, word):
+    enbl_list = [cmd for cmd in word if cmd not in [".disable", ".enable"]]
+    enabled = []
+    
+    # If there's nothing it can enable...
+    if not enbl_list:
+        irc.msg(channel, "{}: No commands to enable.".format(user))
+        return
+    
+    # Remove channel from the command object disabled attribute, if in it.
+    for command in enbl_list:
+        for cmd_name in var.commands:
+            if command in var.commands[cmd_name].aliases:
+                if channel not in var.commands[cmd_name].disabled:
+                    irc.notice(user, "{} is not disabled in this channel.".format(command))
+                else:
+                    var.commands[cmd_name].disabled.remove(channel)
+                    enabled.append(command)
+    
+    # Phooey. Now check if anything was indeed enabled and what.
+    if not enabled:
+        irc.msg(channel, "{}: No commands were enabled.".format(user))
+    else:
+        irc.msg(channel, "{}: Enabled commands: {}".format(user, " ".join(enabled)))
