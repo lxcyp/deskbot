34bb4c4b82107141c650d3c612dabac731b74f87
Josh <joshua.smailes@gmail.com>
2015-05-27 12:41:48 +0100

    Added chat command.
    Couldn't get bot to load, so still needs testing.

---
+++ modules/command_modules/chat.py
@@ -0,0 +1,24 @@
+from .. import irc, var
+from chatterbotapi import ChatterBotFactory, ChatterBotType
+
+# Fill command dictionary.
+def ins_command ():
+    var.commands["chat"] = type("command", (object,), {})()
+    var.commands["chat"].method = chat
+    var.commands["chat"].aliases = [".chat"]
+    var.commands["chat"].usage = [
+        "{} msg - sends msg to cleverbot's api and says the reply in the chat"
+    ]
+
+# Command method.
+def chat (user, channel, word):
+    factory = ChatterBotFactory()
+    bot1 = factory.create(ChatterBotType.CLEVERBOT)
+    bot1session = bot1.create_session()
+    if len(word) == 1:
+        irc.msg(channel, "{}: You need to add a message.".format(user))
+    else:
+        string = " ".join(word[1:])
+        think = bot1session.think(text)
+        
+        irc.msg(channel, "{}: {}".format(user, think))
