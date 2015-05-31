a58656eb695e17661750e84a30c3ef8032147e7b
spoonm <uirsu@hotmail.com>
2015-05-31 17:01:45 -0300

    Changed responses for 'spoonm', cause I'm special.

--- modules/command_modules/love.py
+++ modules/command_modules/love.py
@@ -27,6 +27,25 @@ love_list = [
     "hugs {} in a loving manner."
 ]
 
+# spoonm's anime list!
+anime_list = [
+    "Shigatsu wa Kimi no Uso",
+    "Clannad",
+    "Clannad ~After Story~",
+    "Barakamon",
+    "Gosick",
+    "Bokura wa Minna Kawaisou",
+    "Zankyou no Terror",
+    "So Ra No Wo To",
+    "Angel Beats!",
+    "White Album 2",
+    "The Garden of Words",
+    "Golden Time",
+    "Gekka Shoujo Nozaki-kun",
+    "Love Hina",
+    "Hanasaku Iroha"
+]
+
 # The love function!
 def love (user, channel, word):
     # If nothing other than '.love' is issued, make the user the target.
@@ -37,5 +56,7 @@ def love (user, channel, word):
     
     if target == irc.botnick:
         irc.msg(channel, "{}: y-you too!".format(user))
+    elif target == "spoonm":
+        irc.msg(channel, "\001ACTION watches {} with spoonm.\001".format(random.choice(anime_list)))
     else:
         irc.msg(channel, "\001ACTION " + random.choice(love_list).format(target) + "\001")
