5c3a19363a99fa143f0e64df5f297c6978c080ec
spoonm <spoonm@spoonm.org>
2015-06-27 18:37:53 -0300

    Added .untrack and .track to seen.

--- modules/command_modules/seen.py
+++ modules/command_modules/seen.py
@@ -1,4 +1,6 @@
 from .. import irc, var, ini
+from ..tools import is_identified
+from ..tools import prefix
 import time
 
 # Fill commands dictionary.
@@ -8,10 +10,46 @@ def ins_command ():
     var.commands["seen"].tags = ["other"]
     var.commands["seen"].aliases = [".seen"]
     var.commands["seen"].usage = ["{} user - Be notified of when user was last seen."]
+    
+    var.commands["track"] = type("command", (object,), {})()
+    var.commands["track"].method = track
+    var.commands["track"].tags = ["other"]
+    var.commands["track"].aliases = [".track"]
+    var.commands["track"].usage = ["{} - Start channel .seen tracking."]
+    
+    var.commands["untrack"] = type("command", (object,), {})()
+    var.commands["untrack"].method = untrack
+    var.commands["untrack"].tags = ["other"]
+    var.commands["untrack"].aliases = [".untrack"]
+    var.commands["untrack"].usage = ["{} - Stop channel .seen tracking."]
 
 # Make space for it in var.data.
 def ins_db ():
     var.data["seen"] = ini.fill_dict("seen.ini", "Seen")
+    var.data["not_track"] = ini.fill_list("seen_blist.ini")
+
+# ident function.
+def ident (f):
+    def seen (user, channel, word):
+        if (
+            (user == irc.admin and is_identified(user)) or
+            word[0] in [".seen"]
+        ):
+            f(user, channel, word)
+        elif word[0] in [".untrack", ".track"] and is_identified(user):
+            if prefix(user, channel) in [
+                var.settings["op.prefix"],
+                var.settings["ircop.prefix"],
+                var.settings["owner.prefix"],
+                var.settings["admin.prefix"],
+                var.settings["halfop.prefix"]
+            ]:
+                f(user, channel, word)
+            else:
+                irc.msg(channel, "{}: You need at least hop to do that.".format(user))
+        else:
+            irc.msg(channel, "{}: You don't have admin rights.".format(user))
+    return seen
 
 # Insert message monitor.
 def ins_monitor (message):
@@ -28,6 +66,10 @@ def ins_monitor (message):
         except IndexError:
             channel = message.split(" JOIN ")[1]
         
+        # Stop if the channel is on the untracked list.
+        if channel in var.data["not_track"]:
+            return
+        
         timestamp = " ".join(time.ctime().split(" ")[1:])
         last_seen = ["joining {}.".format(channel), "Time: {}. (GMT-3:00)".format(timestamp)]
         
@@ -37,6 +79,10 @@ def ins_monitor (message):
     # The user leaves a channel.
     if event == "PART":
         channel = message.split(" PART ")[1].split(" :")[0]
+        
+        # Stop if the channel is on the untracked list.
+        if channel in var.data["not_track"]:
+            return
         
         timestamp = " ".join(time.ctime().split(" ")[1:])
         last_seen = ["parting {}.".format(channel), "Time: {}. (GMT-3:00)".format(timestamp)]
@@ -48,6 +94,10 @@ def ins_monitor (message):
     elif event == "PRIVMSG":
         channel = message.split(' ')[2] if message.split(' ')[2] != irc.botnick else user
         content = message.split(' :', 1)[1] if len(message.split(' :')) > 1 else ''
+        
+        # Stop if the channel is on the untracked list.
+        if channel in var.data["not_track"]:
+            return
         
         timestamp = " ".join(time.ctime().split(" ")[1:])
         last_seen = [
@@ -81,3 +131,23 @@ def seen (user, channel, word):
     else:
         irc.msg(channel, "{} was last seen {}".format(word[1], var.data["seen"][word[1]][0]))
         irc.msg(channel, var.data["seen"][word[1]][1])
+
+# track command method.
+def track (user, channel, word):
+    if channel in var.data["not_track"]:
+        var.data["not_track"].remove(channel)
+        ini.remove_from_list(channel, "seen_blist.ini")
+        
+        irc.msg(channel, "{}: .seen will track your channel from now on.".format(user))
+    else:
+        irc.msg(channel, "{}: .seen is already tracking your channel.".format(user))
+
+# untrack command method.
+def untrack (user, channel, word):
+    if channel not in var.data["not_track"]:
+        var.data["not_track"].append(channel)
+        ini.add_to_list(channel, "seen_blist.ini")
+        
+        irc.msg(channel, "{}: .seen won't track your channel from now on.".format(user))
+    else:
+        irc.msg(channel, "{}: .seen is already set not to track your channel.".format(user))
