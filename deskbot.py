6203212cc44b4f329e2c08141c33d9385daba035
spoonm <spoonm@spoonm.org>
2015-07-26 08:42:49 -0300

    Receiving messages made easier. (experimental)

--- deskbot.py
+++ deskbot.py
@@ -1,8 +1,7 @@
 import os
-import sys
-import argparse
 import time
-from modules import ini, irc, commands, var, tools
+import argparse
+from modules import ini, irc, handler, yvar
 from socket import error as socket_error
 
 os.system("cls" if os.name == "nt" else "clear")
@@ -49,79 +48,10 @@ var.settings = {
     for key in var.settings
 }
 
-# Filling commands.
-commands.fill_commands()
-
-# Connecting and display info.
-def connect ():
-    try:
-        irc.connect(irc.server, irc.port)
-    except socket_error:
-        print "Couldn't connect to {}... (Retrying in 15 seconds.)".format(irc.server)
-        
-        # Make irc.ircsock an object with a recv() function.
-        irc.ircsock = type("socket", (object,), {
-            "recv": lambda *x: ""
-        })()
-        
-        # Wait for 15 seconds before attempting to reconnect.
-        time.sleep(15)
-        return
-    
-    irc.display_info()
-    irc.init()
-    
-    # In case there was an error during initial info exchange.
-    if tools.nick_check():
-        # Append _ to botnick and try to reconnect.
-        irc.nick(irc.botnick + "_")
-        
-        # Checking for an error again.
-        if tools.nick_check():
-            irc.quit()
-            print "Probably erroneous nickname. ({}_ didn't work.)".format(irc.botnick)
-            raise SystemExit
-        else:
-            irc.botnick += "_"
-            print "Nick was already in use. Using {} now.".format(irc.botnick)
-
-connect()
-
-# Joining predetermined channels.
-for channel in var.channels:
-    irc.join(channel)
+handler.fill_commands()
+feed.connect()
 
 # Main loop. It's tiem.
 while True:
-    line = irc.ircsock.recv(512)
-    
-    # Check if the connection has been interrupted.
-    if len(line) == 0:
-        print "The bot has been disconnected from the server."
-        print "Attempting to reconnect now..."
-        connect()
-    
-    # Securi-tea...? Iunno, I'm trying to avoid timing attacks.
-    
-    # If there was a split line last time, append to it.
-    if commands.split_line:
-        commands.split_line += line.split("\r\n")[0]            # Complete the split line.
-        line = "\r\n".join(line.split("\r\n")[1:]) + "\r\n"     # Remove it from line.
-        commands.read(commands.split_line, "")                  # Read split line.
-    
-    # Check for split lines.
-    if not (line.endswith("\r\n") or line.endswith("\r")):
-        commands.split_line = line.split("\r\n")[-1]            # Store beginning of split line.
-        line = "\r\n".join(line.split("\r\n")[:-1])             # Remove it from line.
-    
-    if line.endswith("\r"):
-        line = line.rstrip("\r")
-    
-    if line.startswith("\n"):
-        line = line.lstrip("\n")
-    
-    msg_list = [message for message in line.split('\r\n') if message]
-    
-    # Read messages.
-    for message in msg_list:
-        commands.read(message)
+    for message in feed.s_out():
+        handler.read(message)
---
+++ modules/feed.py
@@ -0,0 +1,88 @@
+import irc
+import var
+import tools
+from socket import error as socket_error
+
+# This module's variables.
+split_line = ""
+
+#######################################################
+#    Will connect the bot to the server and deal      #
+#    with nickname errors.                            #
+#######################################################
+
+def connect ():
+    try:
+        irc.connect(irc.server, irc.port)
+    except socket_error:
+        # If the bot can't connect to the server, we get a socket error.
+        print "Couldn't connect to {}... (Retrying in 15 seconds.)".format(irc.server)
+        
+        # Make irc.ircsock an object with recv and send methods.
+        irc.ircsock = type("socket", (object,), {
+            "recv": lambda *_: "",
+            "send": lambda *_: ""
+        })()
+        
+        # Wait for 15 seconds before attempting to reconnect.
+        time.sleep(15)
+        return
+    
+    # In case the connection to the server is successful...
+    
+    irc.display_info()
+    irc.init()
+    
+    # In case the nickname throws errors.
+    while tools.nick_check() and len(irc.botnick) < 20:
+        # Append _ to botnick and try to reconnect.
+        irc.botnick += "_"
+        irc.nick(irc.botnick)
+            
+    # Check one last time for nick errors.
+    if tools.nick_check():
+        print "deskbot tried her best, but couldn't get in with any nicknames."
+        raise SystemExit
+    else:
+        print "Using nickname: {}".format(irc.botnick)
+    
+    print "Joining channels: {}".format(" ".join(var.channels))
+    
+    for channel in var.channels:
+        irc.join(channel)
+
+#######################################################
+#    Will return a list of complete lines received    #
+#    by the server.                                   #
+#######################################################
+
+def s_out ():
+    global split_line
+    global lo_msgs
+    
+    line = irc.ircsock.recv(512)
+    s_arr = []
+    
+    # Check if the connection has been interrupted.
+    if len(line) == 0:
+        print "The bot has been disconnected from the server."
+        print "Attempting to reconnect now..."
+        connect()
+    
+    # Remove leftover '\n'/'\r' from a not that split line.
+    line = line.lstrip("\n")
+    line = line.rstrip("\r")
+    
+    # If there was a split line last time, complete it.
+    if split_line:
+        s_arr.append(split_line + line.split("\r\n")[0])
+        s_arr += line.split("\r\n")[1:-1]
+        split_line = ""
+    else:
+        s_arr += line.split("\r\n")[:-1]
+    
+    # Check for split lines.
+    if not line.endswith("\r\n"):
+        split_line = line.split("\r\n")[-1]
+        
+    return s_arr
