8ca1b6dc633ed301f0915ab262c567449056aa12
spoonm <spoonm@spoonm.org>
2015-08-08 23:25:28 -0300

    Added "--add" functionality.

--- modules/commands/mio.py
+++ modules/commands/mio.py
@@ -1,5 +1,19 @@
 import random
-from .. import irc, var
+from .. import irc, var, ini
+from ..tools import is_identified
+
+def ident (f):
+    
+    def check (user, channel, word):
+        if len(word) > 2 and word[1] in ["add", "-a", "-add", "--add"]:
+            if user == irc.admin and is_identified(user):
+                f(user, channel, word)
+            else:
+                irc.msg(channel, "{}: You don't have admin rights.")
+        else:
+            f(user, channel, word)
+    
+    return check
 
 def ins_command ():
     var.commands["mio"] = type("command", (object,), {})()
@@ -9,8 +23,15 @@ def ins_command ():
     var.commands["mio"].usage = ["{} - Get a random picture of Mio."]
 
 def mio (user, channel, word):
-    f = open("misc/mio.txt", "r")
-    p_list = [l.strip() for l in f.readlines()]
-    f.close()
     
-    irc.msg(channel, "Random picture of Mio: {}".format(random.choice(p_list)))
+    if len(word) > 2 and word[1] in ["add", "-a", "-add", "--add"]:
+        url = word[2]
+        ini.add_to_list(line = url, filename = "misc/mio.txt", raw_path = True)
+        
+        irc.msg(channel, "{}: Line added successfully.".format(user))
+    else:
+        f = open("misc/mio.txt", "r")
+        p_list = [l.strip() for l in f.readlines()]
+        f.close()
+    
+        irc.msg(channel, "Random picture of Mio: {}".format(random.choice(p_list)))
